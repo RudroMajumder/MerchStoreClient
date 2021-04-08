9e4407d7481916b19fa777b0d10758615538b8a8
RudroMajumder <majumder.rudrofficial@gmail.com>
2021-04-08 18:01:18 +0600

    home responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -21,9 +21,9 @@ const Header = () => {
     }
     return (
         <div >
-            <nav class="navbar navbar-expand-lg navbar-light " style={{backgroundColor:"#EFEFEF"}}>
+            <nav className="navbar navbar-expand-lg navbar-light w-100" style={{backgroundColor:"#EFEFEF"}}>
         <Link className="nav-link logo"> Merch Store </Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
 
